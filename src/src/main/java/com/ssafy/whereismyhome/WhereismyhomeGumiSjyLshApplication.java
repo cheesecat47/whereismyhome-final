9e059d16d3ebacd4b16ea0f56cd1b3c3d9bdcc8f
이승현 <sh980812@naver.com>
2023-11-03 13:28:17 +0900

    init

---
+++ src/src/main/java/com/ssafy/whereismyhome/WhereismyhomeGumiSjyLshApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.whereismyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyhomeGumiSjyLshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereismyhomeGumiSjyLshApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/com/ssafy/whereismyhome/WhereismyhomeGumiSjyLshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.whereismyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereismyhomeGumiSjyLshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
