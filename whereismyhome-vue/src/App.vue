29bbd3f23c851ea4862282c3482293dc99fd6af5
hyun812 <sh980812@naver.com>
2023-11-11 19:39:05 +0900

    feat: router 세팅

--- whereismyhome-vue/src/App.vue
+++ whereismyhome-vue/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import TheLoginViewVue from './views/TheLoginView.vue';
+import TheMainView from './views/TheMainView.vue';
+import TheSignUpView from './views/TheSignUpView.vue';
 </script>
 
 <template>
-  <TheLoginViewVue />
+  <RouterView />
+  <!-- <TheLoginViewVue /> -->
+  <!-- <TheSignUpView /> -->
 </template>
 
 <style scoped></style>
--- whereismyhome-vue/src/router/index.js
+++ whereismyhome-vue/src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import TheLoginView from '../views/TheLoginView.vue';
+import TheSignUpView from '../views/TheSignUpView.vue';
+import TheMainView from '../views/TheMainView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'main',
+      component: TheMainView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: TheLoginView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: TheSignUpView,
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: TheLoginView,
     },
   ],
 });
