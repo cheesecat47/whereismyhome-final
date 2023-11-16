2f4fc3e6b55148dbaa7e1bd7c9d0928f68432399
cheesecat47 <cheesecat47@gmail.com>
2023-11-16 14:44:49 +0900

    Refactor: Update configurations

--- app/src/main/java/com/ssafy/whereismyhome/config/WebMvcConfiguration.java
+++ app/src/main/java/com/ssafy/whereismyhome/config/WebMvcConfiguration.java
@@ -6,13 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 @MapperScan(basePackages = {"com.ssafy.whereismyhome.**.mapper"})
 public class WebMvcConfiguration implements WebMvcConfigurer {
     private final Logger logger = LoggerFactory.getLogger(WebMvcConfiguration.class);
@@ -30,8 +31,4 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .maxAge(1800); // 1800초 동안 preflight 결과를 캐시에 저장
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index2");
-    }
 }
