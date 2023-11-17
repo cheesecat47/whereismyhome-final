95528a3473e97481bede78d112bb447c0f44adff
hyun812 <sh980812@naver.com>
2023-11-17 11:43:13 +0900

    feat: 상단 nav bar 추가

--- whereismyhome-vue/src/App.vue
+++ whereismyhome-vue/src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-import TheLoginViewVue from './views/TheLoginView.vue';
-import TheMainView from './views/TheMainView.vue';
-import TheSignUpView from './views/TheSignUpView.vue';
+import TheHeadingNavBar from './components/layout/TheHeadingNavBar.vue';
 </script>
 
 <template>
+  <TheHeadingNavBar />
   <RouterView />
-  <!-- <TheLoginViewVue /> -->
-  <!-- <TheSignUpView /> -->
 </template>
 
 <style scoped></style>
---
+++ whereismyhome-vue/src/components/layout/TheHeadingNavBar.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { ref } from 'vue';
+import { Dialog, DialogPanel, PopoverGroup } from '@headlessui/vue';
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
+
+const mobileMenuOpen = ref(false);
+</script>
+
+<template>
+  <header class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <RouterLink :to="{ name: 'main' }" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        </RouterLink>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <PopoverGroup class="hidden lg:flex lg:gap-x-12">
+        <RouterLink :to="{ name: 'community' }" class="text-sm font-semibold leading-6 text-gray-900"
+          >실거래가조회</RouterLink
+        >
+        <RouterLink :to="{ name: 'community' }" class="text-sm font-semibold leading-6 text-gray-900"
+          >커뮤니티</RouterLink
+        >
+        <RouterLink :to="{ name: 'community' }" class="text-sm font-semibold leading-6 text-gray-900"
+          >공지사항</RouterLink
+        >
+      </PopoverGroup>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <RouterLink :to="{ name: 'login' }" class="text-sm font-semibold leading-6 text-gray-900">로그인</RouterLink>
+        <!-- <a href="/login" class="text-sm font-semibold leading-6 text-gray-900"
+          >로그인 <span aria-hidden="true">&rarr;</span></a
+        > -->
+      </div>
+    </nav>
+    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <RouterLink
+                :to="{ name: 'community' }"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >실거래가조회</RouterLink
+              >
+              <RouterLink
+                :to="{ name: 'community' }"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >커뮤니티</RouterLink
+              >
+              <RouterLink
+                :to="{ name: 'community' }"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >공지사항</RouterLink
+              >
+            </div>
+            <div class="py-6">
+              <RouterLink
+                :to="{ name: 'login' }"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >로그인</RouterLink
+              >
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
