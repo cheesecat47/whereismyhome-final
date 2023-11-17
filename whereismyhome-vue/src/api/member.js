560c0c11296ceb2f2d4ba1727d63aaee7606373e
hyun812 <sh980812@naver.com>
2023-11-17 11:45:59 +0900

    feat: 전체 유저 데이터 가져오는 api 추가

--- whereismyhome-vue/src/api/member.js
+++ whereismyhome-vue/src/api/member.js
@@ -3,9 +3,13 @@ import { localAxios } from '../util/http-commons';
 const local = localAxios();
 
 export const loginMember = (user, success, fail) => {
-  local.post('/login', JSON.stringify(user)).then(success).catch(fail);
+  local.post('/login', { user_id: user.user_id, password: user.password }).then(success).catch(fail);
 };
 
 export const signupMember = (user, success, fail) => {
   local.post('/user', JSON.stringify(user)).then(success).catch(fail);
+};
+
+export const getAllMemberInfo = (success, fail) => {
+  local.get('/user').then(success).catch(fail);
 };
