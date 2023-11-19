bef81fb06b92f61336b55d130a9a8674c2cdd372
hyun812 <sh980812@naver.com>
2023-11-19 21:23:24 +0900

    feat: main 페이지 배경사진 추가

--- frontend/src/views/TheMainView.vue
+++ frontend/src/views/TheMainView.vue
@@ -1,7 +1,16 @@
 <script setup></script>
 
 <template>
-  <div>메인입니다</div>
+  <!-- opacity-80 -->
+  <div>
+    <img src="../../public/image.png" class="relative top-0 w-full h-1/3 object-cover" />
+    <div
+      class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-bold text-slate-400 text-3xl text-center"
+    >
+      <!-- <p>Welcome</p>
+      <p>Where Is My Home</p> -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
