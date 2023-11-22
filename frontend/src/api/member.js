06d22c5e7886fafbb0feae75d86bb24930dc6e8f
hyun812 <sh980812@naver.com>
2023-11-23 01:55:23 +0900

    feat: member 로그인, 회원가입 api 추가

--- frontend/src/api/member.js
+++ frontend/src/api/member.js
@@ -2,14 +2,14 @@ import { localAxios } from '../util/http-commons';
 
 const local = localAxios();
 
-export const loginMember = (user, success, fail) => {
-  local.post('/login', { user_id: user.user_id, password: user.password }).then(success).catch(fail);
+export const loginMember = async (user, success, fail) => {
+  await local.post('/member/login', { email: user.email, password: user.password }).then(success).catch(fail);
 };
 
-export const signupMember = (user, success, fail) => {
-  local.post('/user', JSON.stringify(user)).then(success).catch(fail);
+export const signupMember = async (user, success, fail) => {
+  await local.post('/member/', JSON.stringify(user)).then(success).catch(fail);
 };
 
-export const getAllMemberInfo = (success, fail) => {
-  local.get('/user').then(success).catch(fail);
-};
+// export const getAllMemberInfo = async (success, fail) => {
+//   await local.get('/user').then(success).catch(fail);
+// };
