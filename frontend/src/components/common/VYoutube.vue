c2ad080b8c07a9dff74f5ca45346f5d50afce96a
hyun812 <sh980812@naver.com>
2023-11-22 20:33:22 +0900

    feat: 메인 페이지 유튜브 추가

---
+++ frontend/src/components/common/VYoutube.vue
@@ -0,0 +1,17 @@
+<template>
+  <YouTube src="https://www.youtube.com/watch?v=1aJxwyRKJL0" @ready="onReady" ref="youtube" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import YouTube from 'vue3-youtube';
+
+export default defineComponent({
+  components: { YouTube },
+  methods: {
+    onReady() {
+      this.$refs.youtube.playVideo();
+    },
+  },
+});
+</script>
--- frontend/src/views/TheMainView.vue
+++ frontend/src/views/TheMainView.vue
@@ -1,15 +1,31 @@
-<script setup></script>
+<script setup>
+import VYoutube from '../components/common/VYoutube.vue';
+</script>
 
 <template>
-  <!-- opacity-80 -->
-  <div>
-    <img src="../../public/image.png" class="relative top-0 w-full h-1/3 object-cover" />
+  <div class="relative">
+    <img src="../../public/image.png" class="w-full h-[32rem] object-cover" />
     <div
-      class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-bold text-slate-400 text-3xl text-center"
+      class="isolate absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white text-5xl text-center leading-normal"
     >
-      <!-- <p>Welcome</p>
-      <p>Where Is My Home</p> -->
+      <p>내가 원하는 집을 찾아보세요</p>
+      <p class="text-xl text-right">#아파트 #실거래가 #커뮤니티 #주변정보</p>
+      <p class="text-xl text-right">#차트 #어디 #내집</p>
     </div>
+  </div>
+  <div class="w-full">
+    <div class="flex justify-center mx-auto max-w-6xl pt-24 h-96">
+      <div class="basis-1/2 border border-slate-200">공지사항 보러가기</div>
+      <VYoutube width="427" height="284" />
+      <!-- <div class="basis-1/2 bg-slate-100"></div> -->
+    </div>
+    <div class="flex justify-center mx-auto max-w-6xl pt-24 h-96">
+      <div class="basis-1/2 border border-slate-200">공지사항 보러가기</div>
+      <div class="basis-1/2 bg-slate-100"></div>
+    </div>
+  </div>
+  <div class="w-full h-20">
+    <div>© 2020 Copyright:</div>
   </div>
 </template>
 
