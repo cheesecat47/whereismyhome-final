c6531e619adaa4a9b55f6b7b2d3087ec2243c13a
hyun812 <sh980812@naver.com>
2023-11-23 01:56:46 +0900

    feat: jwt활용 로그인

--- frontend/src/views/TheLoginView.vue
+++ frontend/src/views/TheLoginView.vue
@@ -1,26 +1,32 @@
 <script setup>
 import { ref } from 'vue';
-import { loginMember } from '../api/member';
+import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
+import { useMemberStore } from '../stores/memberStore.js';
 
 const router = useRouter();
+const memberStore = useMemberStore();
+
+const { isLogin } = storeToRefs(memberStore);
+const { userLogin } = memberStore;
+// const { changeMenuState } = useMenuStore();
 
 const user = ref({
-  user_id: '',
+  email: '',
   password: '',
 });
 
-const loginEvent = () => {
-  loginMember(
-    user.value,
-    ({ data }) => {
-      console.log(data);
-      router.push({ name: 'main' });
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+const loginEvent = async () => {
+  console.log(isLogin);
+  await userLogin(user.value);
+  const token = sessionStorage.getItem('accessToken');
+  console.log(token);
+  console.log(isLogin);
+  if (isLogin.value) {
+    router.push({ name: 'main' });
+  } else {
+    alert('로그인에 실패했습니다. 다시입력해주세요');
+  }
 };
 </script>
 
@@ -34,10 +40,10 @@ const loginEvent = () => {
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form v-on:submit.prevent="loginEvent" class="space-y-6">
         <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">아이디</label>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">이메일</label>
           <div class="mt-2">
             <input
-              v-model="user.user_id"
+              v-model="user.email"
               type="text"
               required=""
               class="pl-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-600 sm:text-sm sm:leading-6"
@@ -67,7 +73,6 @@ const loginEvent = () => {
 
         <div>
           <button
-            @click="loginEvent"
             type="submit"
             class="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
           >
