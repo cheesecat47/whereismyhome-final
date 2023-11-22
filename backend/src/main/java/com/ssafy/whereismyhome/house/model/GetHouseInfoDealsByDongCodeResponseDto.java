5c6c49c7b4b1bf2756bfa61af0e5a6daae0f2419
cheesecat47 <cheesecat47@gmail.com>
2023-11-22 14:54:28 +0900

    [BE] fix: HouseController 리팩토링 (#7)

--- backend/src/main/java/com/ssafy/whereismyhome/house/model/GetHouseInfoDealsByDongCodeResponseDto.java
+++ backend/src/main/java/com/ssafy/whereismyhome/house/model/GetHouseInfoDealsByDongCodeResponseDto.java
@@ -3,12 +3,10 @@ package com.ssafy.whereismyhome.house.model;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @ApiModel(value = "GetHouseInfoDealsByDongCodeResponseDto", description = "동 코드로 아파트 최근 거래 목록 목록을 반환할 Dto")
 public class GetHouseInfoDealsByDongCodeResponseDto {
     private int status;
     private String message;
-    private List<HouseInfoDealDto> data;
+    private Object data;
 }
