795fb879f827b325aeee47109fcfe5c17a8fe254
hyun812 <sh980812@naver.com>
2023-11-23 22:41:58 +0900

    feat: footer 추가

---
+++ frontend/src/components/common/VFooter.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <!-- footer -->
+  <div class="bg-[#222222] min-h-72 pt-8">
+    <section class="flex justify-around py-8">
+      <div class="flex items-center">
+        <div class="uppercase text-white text-3xl font-black">where is my home</div>
+      </div>
+
+      <div class="text-white flex gap-14">
+        <div>
+          <!-- <h3 class="capitalize text-xl font-bold">about us</h3> -->
+          <p class="text-gray-300">메인</p>
+          <p class="text-gray-300">실거래가조회</p>
+          <p class="text-gray-300">커뮤니티</p>
+          <p class="text-gray-300">공지사항</p>
+          <p class="text-gray-300">회원가입</p>
+        </div>
+        <div>
+          <h3 class="capitalize font-bold">contact</h3>
+          <p class="text-gray-300">dltmdgus4802@gmail.com</p>
+          <p class="text-gray-300">cheesecat47@gmail.com</p>
+        </div>
+      </div>
+    </section>
+    <hr class="w-4/5 m-auto" />
+    <section class="flex justify-center items-center text-white p-8">
+      <p>Copyright© All Rights Reserved 2023</p>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
