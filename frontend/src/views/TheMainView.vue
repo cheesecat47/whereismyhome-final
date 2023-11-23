eab98e01cbc4b914fefc74eb51eeae20403bad3c
hyun812 <sh980812@naver.com>
2023-11-23 22:43:54 +0900

    feat: 메인 페이지 레이아웃 추가

--- frontend/src/views/TheMainView.vue
+++ frontend/src/views/TheMainView.vue
@@ -1,10 +1,211 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import VFooter from '../components/common/VFooter.vue';
 import VYoutube from '../components/common/VYoutube.vue';
+import { getNotices } from '../api/board';
+import { getFavouriteAptTop5, getFavouriteDongTop5 } from '../api/stats.js';
+
+const callouts = [
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: '../../public/carousel2.jpg',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: '../../public/carousel3.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: '../../public/carousel4.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+  },
+];
+
+const allNoticeData = ref([]);
+
+const dongOption = ref([]);
+const apartOption = ref([]);
+const dongTop5 = ref([]);
+const apartTop5 = ref([]);
+
+onMounted(() => {
+  getNotices(
+    ({ data }) => {
+      allNoticeData.value = data.data.slice(0, 4);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  // 가장 인기있는 동 옵션 설정
+  getFavouriteDongTop5(
+    ({ data }) => {
+      console.log(data);
+      dongTop5.value = data.data;
+      const dongNameArr = data.data.map((item) => {
+        return item['dongName'];
+      });
+      const viewsArr = data.data.map((item) => {
+        return item.views;
+      });
+
+      for (let i = 0; i < 5; i++) {
+        dongOption.value.push({
+          name: dongNameArr[i],
+          y: viewsArr[i],
+        });
+      }
+
+      dongOption.value = {
+        colors: ['rgb(19 78 74)', 'rgb(17 94 89)', 'rgb(15 118 110)', 'rgb(13 148 136)', 'rgb(20 184 166)'],
+        chart: {
+          type: 'pie',
+        },
+        credits: {
+          enabled: false,
+        },
+        title: {
+          text: '가장 인기있는 동 Top 5',
+          align: 'center',
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: false,
+            },
+            showInLegend: true,
+          },
+          series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [
+              {
+                enabled: true,
+                distance: 20,
+              },
+              {
+                enabled: true,
+                distance: -40,
+                format: '{point.percentage:.1f}%',
+                style: {
+                  fontSize: '0.8em',
+                  textOutline: 'none',
+                  opacity: 0.7,
+                },
+                filter: {
+                  operator: '>',
+                  property: 'percentage',
+                  value: 10,
+                },
+              },
+            ],
+          },
+        },
+        series: [
+          {
+            name: '검색수',
+            colorByPoint: true,
+            data: dongOption.value,
+          },
+        ],
+      };
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  // 가장 인기있는 아파트 옵션 설정
+  getFavouriteAptTop5(
+    ({ data }) => {
+      const aptNameArr = data.data.map((item) => {
+        return item.apartmentName;
+        // + ' (' + item.sidoName + ' ' + item.gugunName + ' ' + item.dongName + ')'
+      });
+      const viewsArr = data.data.map((item) => {
+        return item.views;
+      });
+
+      for (let i = 0; i < 5; i++) {
+        apartOption.value.push({
+          name: aptNameArr[i],
+          y: viewsArr[i],
+        });
+      }
+
+      apartOption.value = {
+        colors: ['#000000', '#333333', '#666666', '#999999', '#dddddd'],
+        chart: {
+          type: 'pie',
+        },
+        credits: {
+          enabled: false,
+        },
+        title: {
+          text: '가장 인기있는 아파트 Top 5',
+          align: 'center',
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: false,
+            },
+            // showInLegend: true,
+          },
+          series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [
+              {
+                enabled: true,
+                distance: 20,
+              },
+              {
+                enabled: true,
+                distance: -40,
+                format: '{point.percentage:.1f}%',
+                style: {
+                  fontSize: '0.8em',
+                  textOutline: 'none',
+                  opacity: 0.7,
+                },
+                filter: {
+                  operator: '>',
+                  property: 'percentage',
+                  value: 10,
+                },
+              },
+            ],
+          },
+        },
+        series: [
+          {
+            name: '검색수',
+            colorByPoint: true,
+            data: apartOption.value,
+          },
+        ],
+      };
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
 </script>
 
 <template>
   <div class="relative">
-    <img src="../../public/image.png" class="w-full h-[32rem] object-cover" />
+    <img src="../../public/image.png" class="w-full h-[38rem] object-cover" />
     <div
       class="isolate absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-white text-5xl text-center leading-normal"
     >
@@ -13,20 +214,108 @@ import VYoutube from '../components/common/VYoutube.vue';
       <p class="text-xl text-right">#차트 #어디 #내집</p>
     </div>
   </div>
-  <div class="w-full">
-    <div class="flex justify-center mx-auto max-w-6xl pt-24 h-96">
-      <div class="basis-1/2 border border-slate-200">공지사항 보러가기</div>
-      <VYoutube width="427" height="284" />
-      <!-- <div class="basis-1/2 bg-slate-100"></div> -->
+  <div class="w-full h-full">
+    <!-- 첫번째 컨텐츠 -->
+    <div class="mx-auto max-w-5xl">
+      <div class="py-16 sm:py-24 lg:max-w-none lg:py-24">
+        <div class="flex gap-4">
+          <Highcharts class="basis-1/2 h-full" :options="dongOption"></Highcharts>
+          <div class="basis-1/2 flex flex-col justify-end mb-6">
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">최근 7일간 통계</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">사용자가 많이 검색한 동을 확인해보세요</p>
+            <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-1 sm:gap-y-2 pt-4">
+              <li v-for="item in dongTop5" :key="item.dongCode">
+                <div class="flex items-center gap-x-6">
+                  <div class="w-full flex justify-between items-center">
+                    <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                      {{ item.sidoName }} {{ item.gugunName }} {{ item.dongName }}
+                    </h3>
+                    <p class="text-sm font-semibold leading-6 text-gray-600">검색수 {{ item.views }}</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="flex justify-center mx-auto max-w-6xl pt-24 h-96">
-      <div class="basis-1/2 border border-slate-200">공지사항 보러가기</div>
-      <div class="basis-1/2 bg-slate-100"></div>
+    <!-- 두번째 컨텐츠 -->
+    <div class="bg-gray-100">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <h2 class="text-2xl font-bold text-gray-900">Collections</h2>
+
+          <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            <div v-for="callout in callouts" :key="callout.name" class="group relative">
+              <div
+                class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64"
+              >
+                <img :src="callout.imageSrc" :alt="callout.imageAlt" class="h-full w-full object-cover object-center" />
+              </div>
+              <h3 class="mt-6 text-sm text-gray-500">
+                <a :href="callout.href">
+                  <span class="absolute inset-0" />
+                  {{ callout.name }}
+                </a>
+              </h3>
+              <p class="text-base font-semibold text-gray-900">{{ callout.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="w-full h-20">
-    <div>© 2020 Copyright:</div>
+    <!-- 세번째 컨텐츠 -->
+    <div class="w-full max-w-5xl mx-auto bg-white py-24 sm:py-32">
+      <div class="flex gap-6">
+        <div class="basis-1/2 px-6 lg:px-8 mt-24">
+          <h2 class="text-2xl font-bold tracking-tight text-gray-900">공지사항</h2>
+          <p class="mt-6 text-lg leading-8 text-gray-600">
+            각종 공지사항, 공고와 새로운 소식을 제공하는 공간입니다. 원활한 사이트 이용을 위해 공지사항을 확인해주세요.
+          </p>
+          <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-1 sm:gap-y-2 pt-4">
+            <li v-for="item in allNoticeData" :key="item.boardId">
+              <div class="flex items-center gap-x-6">
+                <div class="w-full flex justify-between items-center">
+                  <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ item.title }}</h3>
+                  <p class="text-sm font-semibold leading-6 text-gray-600">{{ item.createdTime.split(' ')[0] }}</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- <VYoutube width="520" height="280" /> -->
+        <Highcharts class="flex-1 h-full" :options="apartOption"></Highcharts>
+      </div>
+    </div>
+
+    <VFooter />
   </div>
 </template>
 
 <style scoped></style>
+
+<!-- 
+<div class="bg-gray-100">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+      <h2 class="text-2xl font-bold text-gray-900">유튜브 추천 영상</h2>
+      <VYoutube width="427" height="280" />
+      // <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+        <div v-for="callout in callouts" :key="callout.name" class="group relative">
+          <div
+            class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64"
+          >
+            <img :src="callout.imageSrc" :alt="callout.imageAlt" class="h-full w-full object-cover object-center" />
+          </div>
+          <h3 class="mt-6 text-sm text-gray-500">
+            <a :href="callout.href">
+              <span class="absolute inset-0" />
+              {{ callout.name }}
+            </a>
+          </h3>
+          <p class="text-base font-semibold text-gray-900">{{ callout.description }}</p>
+        </div>
+      </div> //
+    </div>
+  </div>
+</div> -->
